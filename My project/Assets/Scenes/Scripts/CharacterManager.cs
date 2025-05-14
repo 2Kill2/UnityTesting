00cd63fef0a62aafa65ccf58769d5e2a653b425b
HuneyDev <64155388+2Kill2@users.noreply.github.com>
2025-05-14 15:40:45 -0700

    updated scritps for lab 2 assignment

--- My project/Assets/Scenes/Scripts/CharacterManager.cs
+++ My project/Assets/Scenes/Scripts/CharacterManager.cs
@@ -2,17 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
 public class Charactermanager : MonoBehaviour
 {
+    private GameObject playerobject;
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerobject = this.gameObject;
+        Debug.Log("Player object set to: " + playerobject.name);
+        Debug.Log(playerobject.transform.position.x + playerobject.transform.position.y + playerobject.transform.position.z);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
--- My project/Assets/Scenes/Scripts/GameManager.cs
+++ My project/Assets/Scenes/Scripts/GameManager.cs
@@ -1,13 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+
 
 public class GameManager : MonoBehaviour
 {
+
+    public GameObject playerPrefab;
+    public GameObject _playerInput;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
